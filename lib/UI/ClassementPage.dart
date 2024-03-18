6d9ae7a2c15a4729e82c466b0fd4d24f7c29fa76
araviart <alexandre.raviart@etu.univ-orleans.fr>
2024-03-18 14:59:06 +0100

    init squelette

---
+++ lib/UI/ClassementPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClassementPage extends StatelessWidget {
+  const ClassementPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text('Page 1'));
+  }
+}
